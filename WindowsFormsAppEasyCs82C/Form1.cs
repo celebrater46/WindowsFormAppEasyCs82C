fd0fe1ea601716cb68301335ab4542e15263543e
hideru <jota.16.stigma.world@gmail.com>
2021-11-06 20:49:56 +0900

    Wrote Codes

---
+++ WindowsFormsAppEasyCs82C/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppEasyCs82C
+{
+    public partial class Form1 : Form
+    {
+        private Image img;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            img = Image.FromFile("C:\\Users\\Enin\\RiderProjects\\WindowsFormsAppEasyCs82C\\WindowsFormsAppEasyCs82C\\img\\sample.jpg");
+            this.Text = "Graphic Cliping";
+            this.ClientSize = new Size(400, 300);
+            this.BackColor = Color.Black;
+
+            this.Paint += new PaintEventHandler();
+        }
+
+        public void FormPaint(Object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            GraphicsPath gp = new GraphicsPath();
+            
+            gp.AddEllipse(new Rectangle(0, 0, 400, 300)); // Adding Ellipse
+            Region rg = new Region(gp);
+            g.Clip = rg;
+            
+            g.DrawImage(img, 0, 0, 400, 300);
+        }
+    }
+}
